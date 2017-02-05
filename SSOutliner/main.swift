7468de56f02aee7f40b7cb02f696808d68e58268
Mikey Ward <mw@mikey.io>
2017-02-04 19:07:09 -0500

    Forces output to separate file for nondestructive outlining

--- SSOutliner/main.swift
+++ SSOutliner/main.swift
@@ -8,24 +8,30 @@
 
 import Cocoa
 
+// Read args (super-naive, ugh)
 let args = ProcessInfo().arguments
 guard args.count > 1 else {
     printUsage()
     exit(EXIT_FAILURE)
 }
-
 let shouldRound = args.contains("-r") || args.contains("--rounded")
 let cornerRadius: CGFloat = shouldRound ? 5.0 : 0.0
-
 var url = URL(fileURLWithPath: args.last!)
-
 guard let img = NSImage(contentsOf: url) else {
     print("Can't load image at \(url)")
     exit(EXIT_FAILURE)
 }
 
+// Add the outline
 let newImg = img.addingOutline(thickness: 1,
                                color: NSColor.black,
                                cornerRadius: cornerRadius)
+
+// Save outlined image
 let pngData = newImg.PNGRepresentation
-try! pngData.write(to: url)
+var newURL = url.deletingPathExtension()
+let name = newURL.lastPathComponent
+newURL.deleteLastPathComponent()
+newURL.appendPathComponent("\(name)_ssoutlined")
+newURL.appendPathExtension("png")
+try! pngData.write(to: newURL)
