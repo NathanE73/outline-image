36a6c41ca6cb962202020febf0ba9867a381f35f
Mark Dalrymple <markd@bignerdranch.com>
2017-07-29 10:56:01 -0400

    Adds --overwrite. Adds --help. Extracts argument processing into here.

--- SSOutliner/helpers.swift
+++ SSOutliner/helpers.swift
@@ -13,5 +13,47 @@ func printUsage() {
     print("flags:")
     print("  -r, --rounded")
     print("      Round the corners. Suitable for window screenshots.")
-    print("\nOutput overwrites input file.\n")
+    print("  -o, --overwrite")
+    print("      Overwrite the input file.")
+    print("If you don't supply -o, output is written to originalfilename_ssoutlined.png")
 }
+
+
+struct Instructions {
+    let shouldRound: Bool
+    let shouldOverwrite: Bool
+    let fileURLs: [URL]
+}
+
+
+func readArgs(_ args: [String]) -> Instructions {
+    var shouldRound = false
+    var shouldOverwrite = false
+    var files: [String] = []
+    
+    var args = args
+    _ = args.removeFirst() // eat the process name
+    
+    while !args.isEmpty {
+        guard let thing = args.popLast() else { continue }
+        if thing.contains("-r") || thing.contains("--rounded") {
+            shouldRound = true
+            continue
+        }
+        if thing.contains("-o") || thing.contains("--overwrite") {
+            shouldOverwrite = true
+            continue
+        }
+        if thing.contains("-o") || thing.contains("--help") {
+            printUsage()
+            exit(0)
+        }
+        files.append(thing)
+    }
+    
+    return Instructions(shouldRound: shouldRound,
+                        shouldOverwrite: shouldOverwrite,
+                        fileURLs: files.map { URL(fileURLWithPath: $0) } )
+}
+
+
